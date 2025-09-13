a538a3635cbdf37d135963a949af2a84d00d2e9a
TheCoder77-web <ruienc@nycstudents.net>
2025-09-13 19:31:28 +0000

    more

--- Java-3b-CW/Java-3b-CW/Main.java
+++ Java-3b-CW/Java-3b-CW/Main.java
@@ -26,7 +26,7 @@ class Main {
     System.out.println("\t\tMary");
     
     // print one, two and three on separate lines using a single line of code
-    System.out.println("\none \ntwo \nthree");
+    System.out.println("one \ntwo \nthree");
 	
   }
   
