ce278a9dee1275d9ecae916930b1bcdad50f3c6b
TheCoder77-web <ruienc@nycstudents.net>
2025-09-17 16:23:26 +0000

    4 activity

---
+++ Java-4-Activity/Main.java
@@ -0,0 +1,66 @@
+class Main {
+	public static void main(String[] args) {
+    	(new Main()).init();
+	}
+
+ void init(){
+// Define a variable for each situation in the challenges below. Assign a value to each variable.
+// Display the value of each variable to the console.
+
+/*  Challenge 1:    
+    1) student's age 
+    2) number of people that live with you
+    3) number of items in a cart 
+*/
+   int age = 16;
+   System.out.println(age);
+   int members = 4;
+   System.out.println(members);
+   int items = 10;
+   System.out.println(items);
+
+
+/*  Challenge 2:
+    1) temperature 
+    2) student's GPA
+    3) growth rate of your investment
+*/
+
+    double temp = 37.4;
+    System.out.println(temp);
+    double gpa = 97.8;
+    System.out.println(gpa);
+    double rate = 1.087;
+    System.out.println(rate);
+
+/*  Challenge 3:
+    1) Student's last name 
+    2) student's first initial
+    3) description of an item 
+*/
+
+    String lName = "Cao";
+    System.out.println(lName);
+    char fInitial = 'R';
+    System.out.println(fInitial);
+    String desc = "Used for cleaning";
+    System.out.println(desc);
+
+/*  Challenge 4:
+    1) the status whether lights are on
+    2) the status whether the game is over
+    3) the status whether the battery is charging
+*/
+
+    boolean isLightsOn = true;
+    System.out.println(isLightsOn);
+    boolean isGameOver = false;
+    System.out.println(isGameOver);
+    boolean isBatteryCharging = false;
+    System.out.println(isBatteryCharging);
+
+    // **************************************************
+    // **** Don't write any code below here.  ***********
+    // **************************************************
+  }
+}
