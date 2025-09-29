78e3aa425ad6db406d39af5dd0792ee634ae47d5
TheCoder77-web <ruienc@nycstudents.net>
2025-09-29 20:12:24 +0000

    7

--- Java-7-CW/Main.java
+++ Java-7-CW/Main.java
@@ -17,13 +17,15 @@ class Main {
     Problem 2: Translate the formula from eq2.png.
   */
 
-
+  double y = (2*3)/Math.sin(Math.toRadians(3*4));
+  System.out.println("y = " + y);
     
     /*
     Problem 3: Translate the formula from eq3.png.
   */
 
-    
+  double b = Math.abs(Math.pow(2,2) + 1);
+  System.out.println("b = " + b);
 
   }
 
