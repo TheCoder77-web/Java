c2a2eb9842e17a6f00feaf2652c62ef4f16cadf0
TheCoder77-web <ruienc@nycstudents.net>
2025-10-30 21:24:25 +0000

    16

--- Java-16-CW/Main.java
+++ Java-16-CW/Main.java
@@ -6,11 +6,11 @@ class Main {
   void printt(Object o){ System.out.print(o);}
 
   void init(){
-   
-    
-  
-
+    String msg = Input.readFile("paragraph.txt");
+    print(numOfWords(msg) + " words");   
+    print(countVowels(msg) + " vowels");
   }
+  
   // For each problem read from the file paragraph.txt .
 
   /*
@@ -19,6 +19,15 @@ class Main {
   Example: "To be or not to be"  ==> 6
   */
 
+    int numOfWords(String str){
+    int cnt = 1;
+    for(int x=0; x<=str.length()-1; x++){
+      String sybl = str.substring(x, x+1);
+      if(sybl.equals(" "))
+        cnt++;
+    }
+    return cnt;
+  }
 
   /*
   Problem 2:
@@ -26,6 +35,14 @@ class Main {
   Example: countVowels("Too be or not to be") ==> 7
   */
 
-  
+    int countVowels(String str){
+    int cnt = 0;
+    for(int x=0; x<=str.length()-1; x++){
+      String cv = str.substring(x, x+1);
+      if(cv.equalsIgnoreCase("a") || cv.equalsIgnoreCase("e") || cv.equalsIgnoreCase("i") || cv.equalsIgnoreCase("o") || cv.equalsIgnoreCase("u") )
+        cnt++;
+    }
+    return cnt;
+  }
   
 }
