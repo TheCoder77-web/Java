9d6f7d1a2d692f9a7733afb9f2b54dfa57e647b2
TheCoder77-web <ruienc@nycstudents.net>
2025-11-25 21:53:28 +0000

    24

--- Java-24-CW/Main.java
+++ Java-24-CW/Main.java
@@ -6,20 +6,48 @@ class Main {
   void printt(Object o){ System.out.print(o);}
 
   void init(){
-    
+    print(toUpper("Have a Nice Day"));
+    print(pswdGenerator());
   }
   /*
   Problem 1:
   Write a fn toUpper() that accepts a string and converts all lower case letters to upper case and returns the result.
   */
 
+    String toUpper(String c){
+      char ch ='0';
+      int ascii = 0;
+      String build = "";
+    
+      for(int x=0; x<=c.length()-1; x++){
+        ch = c.charAt(x);
+        ascii = (int)ch;
+        if (ascii>=97 && ascii<=122){ 
+          ascii -= 32;
+          ch = (char) ascii;
+        }
+        build += ch;
+      }
+      return build;
+    }
 
   /*
   Problem 2:
   Write a fn pswdGenerator() that generates and returns a 9-character password.
 */
 
-
+    String pswdGenerator(){   
+      char ch = '0';
+      int ascii = 0;
+      String build=""; 
+    
+      for(int x=1; x<=9; x++){
+        ascii = randInt(33,126); 
+        ch = (char) ascii;
+        build += ch;
+      }
+      return build;
+    }
   
 
   //fn to generate random integer
